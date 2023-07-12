174a2aafafb749488dfea6278bb7420f8f85d723
Flepsz <luisfelipelfsp3@gmail.com>
2023-07-12 09:14:02 -0300

    CRA added

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,11 +1,29 @@
-import {} from "express";
+import { Request, Response} from "express";
+import EmailService from "../services/EmailService";
 
 const users = [
-    { name: "Luis", email: "lusifelp@gmail.com"}
+	{ name: "Luis", email: "lusifelp@gmail.com"}
 ];
 
 export default {
-    async index(req, res) {
+    async index(req: Request, res: Response) {
         return res.json(users);
-    }
+    },
+
+	async create(req: Request, res: Response) {
+		const emailService = new EmailService();
+		
+		emailService.sendMail({
+			to: {
+				name: "Felipe Pereira",
+				email: "lusifelp@gmail.com"
+			},
+			message: {
+				subject: "Bem vindo!",
+				body: "Aprendendo TypeScript!"
+			}
+		});
+
+		return res.send()
+	}
 };
